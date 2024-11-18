83bff177186ef14b06120c24ec6f95b517297533
Student <Student@host.docker.internal>
2024-11-18 13:01:24 +0100

    co

--- Praktikum2/src/business/MoebelnModel.java
+++ Praktikum2/src/business/MoebelnModel.java
@@ -6,6 +6,11 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import reader.ConcreteCSVCreator;
+import reader.ConcreteTXTCreator;
+import reader.Creator;
+import reader.Product;
+
 public class MoebelnModel {
 	private Moebeln moebeln;
 	
@@ -24,12 +29,24 @@ public class MoebelnModel {
 	 	}
 	
 	public void leseAusDatei(String typ) throws IOException {
-		if ("csv".equals(typ)) {
-			BufferedReader ein = new BufferedReader(new FileReader("MoebelnAusgabe.csv"));
-			String[] zeile = ein.readLine().split(";");
-			this.moebeln = new Moebeln(zeile[0], Float.parseFloat(zeile[1]), zeile[2], Float.parseFloat(zeile[3]), zeile[4].split(";"));
-			ein.close();
+//		if ("csv".equals(typ)) {
+//			BufferedReader ein = new BufferedReader(new FileReader("MoebelnAusgabe.csv"));
+//			String[] zeile = ein.readLine().split(";");
+//			this.moebeln = new Moebeln(zeile[0], Float.parseFloat(zeile[1]), zeile[2], Float.parseFloat(zeile[3]), zeile[4].split(";"));
+//			ein.close();
+//		}
+		Creator creator = null;
+		if(typ.equals("csv")) {
+			creator = new ConcreteCSVCreator();
 		}
+		else {
+			creator = new ConcreteTXTCreator();
+		}
+		
+		Product reader = creator.factoryMethod(typ);
+		
+		String[] zeile = reader.leseAusDatei();
+		this.moebeln = new Moebeln(zeile[0], Float.parseFloat(zeile[1]), zeile[2], Float.parseFloat(zeile[3]), zeile[4].split(";"));
 	}
 
 }
